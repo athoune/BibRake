bae409f20f0cb5024ba8072e0264678377621e8d
Mathieu Lecarme <mathieu@garambrogne.net>
2009-08-04 17:33:15 +0200

    poc

---
+++ Bib.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'treetop'
+
+Treetop.load "BibTex"
+
+parser = BibTexParser.new
+
+f = File.open('bibliographie.bib')
+parser.parse(f.read).elements.each do |book|
+	puts book
+end
