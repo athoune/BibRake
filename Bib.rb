e11a310bd5a9383bb2f735354e54262a7deedcb8
Mathieu Lecarme <mathieu@garambrogne.net>
2009-09-02 20:13:42 +0200

    book

--- Bib.rb
+++ Bib.rb
@@ -10,21 +10,27 @@ parser = BibTexParser.new
 
 f = File.open('bibliographie.bib')
 #tree = parser.parse(f.read)
+
+=begin
+=end
 txt = <<x
 	@BOOK{Zienkiewicz2,
-	  title = {The Finite Element Method},
-	  publisher = {Butterworth-Heinemann},
-	  year = {2000},
-	  author = {O.C. Zienkiewicz and R.L. Taylor},
-	  volume = {Solid Mechanics},
-	  edition = {Fifth Edition},
-	  file = {:home/lecarme/Documents/Biblio/Books and thesis/Finite Element Method
+		title = {The Finite Element Method},
+		publisher = {Butterworth-Heinemann},
+		year = {2000},
+		author = {O.C. Zienkiewicz and R.L. Taylor},
+		volume = {Solid Mechanics},
+		edition = {Fifth Edition},
+		file = {:home/lecarme/Documents/Biblio/Books and thesis/Finite Element Method
 		- Solid mechanics - Zienkiewicz and Taylor.pdf:PDF},
-	  owner = {lecarme},
-	  timestamp = {27.01.2009}
+		owner = {lecarme},
+		timestamp = {27.01.2009}
 	}
+
+
 x
-txt = "\n\nplop\n{toto}\n@BOOK\n"
+#txt = "    beuha= {aussi\n heing?}, machin = truc\n\n, pim = {pam\n}"
+#txt = "Zienkiewicz2"
 tree = parser.parse(txt)
 puts txt, tree
 tree.elements.each do |element|
