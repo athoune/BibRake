f7912dad8032a240780c1c647f33f3e36a2c481a
Mathieu Lecarme <mathieu@garambrogne.net>
2009-09-04 18:48:32 +0200

    it parses

--- Bib.rb
+++ Bib.rb
@@ -14,7 +14,10 @@ f = File.open('bibliographie.bib')
 =begin
 =end
 txt = <<x
-	@BOOK{Zienkiewicz2,
+% This file was created with JabRef 2.3.1.
+% Encoding: UTF-8
+
+@BOOK{Zienkiewicz2,
 		title = {The Finite Element Method},
 		publisher = {Butterworth-Heinemann},
 		year = {2000},
@@ -27,12 +30,39 @@ txt = <<x
 		timestamp = {27.01.2009}
 	}
 
+	@BOOK{Anderson2005,
+	  title = {Fracture mechanics, fundamentals and applications},
+	  publisher = {CRC Press},
+	  year = {2005},
+	  editor = {Taylor and Francis group},
+	  author = {Anderson, Ted L.},
+	  edition = {3rd},
+	  owner = {lecarme},
+	  timestamp = {18.05.2009}
+	}
+@BOOK{Abaqus,
+  title = {ABAQUS Theory Manual Version 6.3},
+  publisher = {Hibbitt, Karlsson and Sorenson Inc.},
+  year = {2002},
+  owner = {lecarme},
+  timestamp = {12.06.2009}
+}
+
+@comment{jabref-meta: selector_publisher:}
+
+@comment{jabref-meta: selector_author:}
+
+@comment{jabref-meta: selector_journal:}
+
+@comment{jabref-meta: selector_keywords:}
+
 
 x
 #txt = "    beuha= {aussi\n heing?}, machin = truc\n\n, pim = {pam\n}"
 #txt = "Zienkiewicz2"
+txt = f.read
 tree = parser.parse(txt)
-puts txt, tree
+puts tree
 tree.elements.each do |element|
 	puts element, element.text_value
 	#.inspect
